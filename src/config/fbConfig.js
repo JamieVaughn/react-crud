a2ae186a13b380907ed355424ddc069c0d29387f
JamieVaughn <wxv6@case.edu>
2020-04-02 14:58:05 -0400

    Remove access info from config

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -2,18 +2,19 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
 
-console.log(process.env.REACT_FIRESTORE_APIKEY)
 // Your web app's Firebase configuration
 export const fbConfig = {
-    apiKey: 'AIzaSyCLxdL8sghwkbps41dVtF-ggYm1mSXQ2Y8',
-    authDomain: 'rf-bookmarks.firebaseapp.com',
-    databaseURL: "https://rf-bookmarks.firebaseio.com",
+    apiKey: process.env.REACT_APP_FIRESTORE_APIKEY,
+    authDomain: process.env.REACT_APP_DOMAIN,
+    databaseURL: process.env.REACT_APP_DB_URL,
     projectId: "rf-bookmarks",
     storageBucket: "rf-bookmarks.appspot.com",
     messagingSenderId: "89112768323",
     appId: "1:89112768323:web:6acb8c0973b9098ebd49f6",
     measurementId: "G-T0L6HC2SHC"
   };
+console.log(fbConfig)
+
   // Initialize Firebase
   firebase.initializeApp(fbConfig);
 //   firebase.analytics();
